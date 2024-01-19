253afe445ac541fb3a5fcdc33969ef0d3da47848
TaiseiNemoto <nemoto@defide-ix.com>
2024-01-19 18:35:33 +0900

    [add]動的ルートのテストコード作成

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+type Geo = {
+  lat: string;
+  lng: string;
+}
+
+type Address = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+const data: User[] = await $fetch('https://jsonplaceholder.typicode.com/users')
+</script>
+
+<template>
+  <ul>
+    <li v-for="user in data">
+      <NuxtLink :to="`/user/${user.id}`">{{ user.name }}</NuxtLink>
+    </li>
+  </ul>
+</template>
---
+++ pages/user/[id].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const route = useRoute()
+
+</script>
+
+<template>
+  <p>hello {{ $route.params.id }}</p>
+</template>
