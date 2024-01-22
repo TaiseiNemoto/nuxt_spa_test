8ac34c78e0cde0dbb3b0dc7cf1049b643a308513
TaiseiNemoto <nemoto@defide-ix.com>
2024-01-22 15:28:51 +0900

    動的ルーティングのページが存在しない場合に、404にフォールバックするように修正

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -1,8 +1,75 @@
 <script setup lang="ts">
+type Geo = {
+  lat: string;
+  lng: string;
+}
+
+type Address = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+type User = {
+  id: number | null;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+const user: Ref<User> = ref({
+  id: null,
+  name: '',
+  username: '',
+  email: '',
+  address: {
+    street: '',
+    suite: '',
+    city: '',
+    zipcode: '',
+    geo: {
+      lat: '',
+      lng: '',
+    }
+  },
+  phone: '',
+  website: '',
+  company:{
+    name: '',
+    catchPhrase: '',
+    bs: '',
+  }
+})
+
 const route = useRoute()
 
+onMounted(async () => {
+  const route = useRoute()
+  try {
+    const data: User = await $fetch(`https://jsonplaceholder.typicode.com/users/${route.params.id}`)
+    user.value = data
+  } catch (err) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Page Not Found',
+      fatal: true
+    })
+  }
+})
 </script>
 
 <template>
-  <p>hello {{ $route.params.id }}</p>
+  <p>hello {{ user.name }}</p>
 </template>
